767846c25727626bc4657ef76e6ed3c457a11772
Eric Day <eric.ch.day@gmail.com>
2023-11-17 09:32:48 -0700

    sec 5 done

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, StyleSheet, Text, Image } from 'react-native';
+
+const ImageDetail = ({ title, imageSource, score }) => {
+  return (
+    <View>
+      <Image source={imageSource} />
+      <Text>{title}</Text>
+      <Text>Image Score - {score}</Text>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({})
+
+export default ImageDetail
+
